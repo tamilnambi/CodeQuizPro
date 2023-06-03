ad7ad4fbe5e41c3ba686f86fe9f7b0cf0df74e75
tamilnambi <123074613+tamilnambi@users.noreply.github.com>
2023-06-03 23:39:59 +0530

    working basic model
    input are retrieved from json and no of correct answers shown as alert

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,20 +1,42 @@
-const correctAns = ['a','a'];
-var ans = [];
-var count = 0;
-var one = document.getElementsByName("one");
-function checkAnswers() {
-    for(let i=0;i < one.length;i++)
-    {
-        if(one[i].checked == true)
-        {
-            ans[count] = one[i].value;
-            count++;
-        }
-    }
-    for(let j=0;j<ans.length;j++)
-    {
-        if(ans[j] === correctAns[j])
-            alert("correct! "+ans[j])
-    }
-    ans = [];
+var answers = [];
+var data;
+async function getData() {
+  var resp = await fetch("src/data/test.json");
+
+  data = await resp.json();
+
+  q = document.getElementsByClassName("questions");
+
+  for (let i = 0; i < data.length; i++) {
+    q[i].innerHTML = `<p> ${i + 1} . ${
+      data[i].question
+    } </p> <input type="radio" name="${i + 1}" value="a">${data[i].a} <br>
+<input type="radio" name="${i + 1}" value="b">${
+      data[i].b
+    } <br> <input type="radio" name="${i + 1}" value="c">${data[i].c} <br>
+<input type="radio" name="${i + 1}" value="d">${data[i].d}`;
+  }
 }
+
+function checkAnswers() {
+  var a;
+  for (let i = 0; i < data.length; i++) {
+    a = document.getElementsByName(`${i + 1}`);
+    for (let j = 0; j < 4; j++) {
+      if (a[j].checked) {
+        answers[i] = a[j].value;
+      }
+    }
+  }
+  result(answers);
+}
+
+function result(answers) {
+  let count = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].correctAns == answers[i]) count++;
+  }
+  alert("correct answers: " + count);
+}
+
+getData();
